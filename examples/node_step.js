24aa658a317199595359763b1dcd20eedf8f5e5d
kovacsv <viktorkovacs@gmail.com>
2022-01-28 22:16:57 +0100

    Add GitHub action to build wasm.

---
+++ examples/node_step.js
@@ -0,0 +1,9 @@
+let fs = require ('fs');
+const occtimportjs = require ('../dist/occt-import-js.js')();
+
+occtimportjs.then ((occt) => {
+	let fileUrl = '../test/testfiles/simple-basic-cube/cube.stp';
+	let fileContent = fs.readFileSync (fileUrl);
+	let result = occt.ReadStepFile (fileContent);
+	console.log (result);
+});
--- test/node_step.js
+++
@@ -1,9 +0,0 @@
-let fs = require ('fs');
-const occtimportjs = require ('../dist/occt-import-js.js')();
-
-occtimportjs.then ((occt) => {
-	let fileUrl = 'testfiles/simple-basic-cube/cube.stp';
-	let fileContent = fs.readFileSync (fileUrl);
-	let result = occt.ReadStepFile (fileContent);
-	console.log (result);
-});
