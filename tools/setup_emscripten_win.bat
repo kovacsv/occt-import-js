bf709c8093b6ae73a8ef768c5f597e61dd1b06a3
kovacsv <viktorkovacs@gmail.com>
2022-12-29 14:44:28 +0100

    Update emscripten and mingw.

--- tools/setup_emscripten_win.bat
+++ tools/setup_emscripten_win.bat
@@ -2,10 +2,10 @@ pushd %~dp0\..
 
 call git clone https://github.com/emscripten-core/emsdk.git
 call cd emsdk
-call emsdk install 3.1.10
-call emsdk activate 3.1.10
-call emsdk install mingw-4.6.2-32bit
-call emsdk activate mingw-4.6.2-32bit
+call emsdk install 3.1.28
+call emsdk activate 3.1.28
+call emsdk install mingw-7.1.0-64bit
+call emsdk activate mingw-7.1.0-64bit
 call cd ..
 
 popd
