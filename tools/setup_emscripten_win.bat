fa61c903087415d4b6a8279a97897f2ea8918db3
kovacsv <viktorkovacs@gmail.com>
2023-06-19 19:36:09 +0200

    emscripten: use wasm-exceptions for better performance #29

--- tools/setup_emscripten_win.bat
+++ tools/setup_emscripten_win.bat
@@ -2,8 +2,8 @@ pushd %~dp0\..
 
 call git clone https://github.com/emscripten-core/emsdk.git
 call cd emsdk
-call emsdk install 3.1.28
-call emsdk activate 3.1.28
+call emsdk install 3.1.35
+call emsdk activate 3.1.35
 call emsdk install mingw-7.1.0-64bit
 call emsdk activate mingw-7.1.0-64bit
 call cd ..
